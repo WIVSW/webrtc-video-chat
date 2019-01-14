f30261ad25f0d4dc38f0f2cc7b3f784c361a1aac
WIVSW <olegspostnikov@gmail.com>
2019-01-14 12:33:23 +0500

    Add webpack dev server

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -1,5 +1,32 @@
-const { entry } = require('./common.config');
+const path = require('path');
+const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const cwd = process.cwd();
+
 
 module.exports = {
-	entry,
+	entry: {
+		index: path.resolve(cwd, 'src/ui/index.js'),
+		vendor: ['react', 'react-dom'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devServer: {
+		host: '192.168.0.101', // Defaults to `localhost`
+		port: 9000, // Defaults to 8080
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/ui/index.html',
+		}),
+		new HotModuleReplacementPlugin(),
+	],
 };
